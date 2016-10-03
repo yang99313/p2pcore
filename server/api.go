4655a23b3182ae4c1c21f37f833080360868a349
metalwood <377307289@qq.com>
2016-10-03 09:34:45 +0800

    update

--- server/api.go
+++ server/api.go
@@ -1,17 +1,21 @@
-func leaderChangedCallback(serverId string) // leader变化了就会触发这个回调
+package p2pcore
 
-func p2pServerStartup(serverId string, servername string, listenAddr string, otherServerAddr string, leaderChangedCallback cb) (error)
+func masterChangedCallback(masterServerId string) // leader变化了就会触发这个回调
+
+func p2pServerStartup(serverId string, serverName string, listenAddr string, otherServerAddr string, leaderChangedCallback cb) (err error)
 
 func p2pServerCleanup()
 
-func p2pServerLookupMyId()(serverId string)
+func p2pServerLookupMyId() (serverId string)
 
-func p2pServerLookupServers()(masterId string, serverIdList []string, error)
+func p2pServerLookupServers() (masterServerId string, serverIdList []string, err error)
 
-func p2pServerLookupClientInfo(clientId string) (wanIp []string, lanIp []string, listenIp string, listenPort int, error)
+func p2pServerLookupClients() (clientIdList []string, err error)
 
-func p2pServerLookupClients()(clientIdList []string, error)
+func p2pServerLookupClientInfo(clientId string) (wanIp []string, lanIp []string, listenIp string, listenPort int, err error)
 
-func p2pServerSend(dstId string, data []byte)(error)
+func p2pServerSend(dstId string, data []byte) (err error)
 
-func p2pServerRecv()(srcId string, data []byte)
+func p2pServerSendMaster(data []byte) (err error)
+
+func p2pServerRecv() (srcId string, srcRole string, data []byte, err error)
